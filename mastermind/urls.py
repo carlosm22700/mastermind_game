e465688a8559b8e2d9f6423829520df1836f4718
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-11-16 12:55:17 -0800

    set up DRF boilerplate

--- mastermind/urls.py
+++ mastermind/urls.py
@@ -17,7 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.urls import include, path
+from rest_framework import routers
+from main_app import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('', include('main_app.urls'))
+
 ]
